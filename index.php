f3b6449002ffc7eb188177b88f26966daca9451a
anvarbeek <anvar1on007@gmail.com>
2024-08-07 18:54:51 +0500

    logo yuklash

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <? include 'header.php';?>
 <div class="conteiner">
-
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="navbar-brand">
+            <img src="logo.png" alt="" >
+        </div>
+    </nav>
 
 
 
